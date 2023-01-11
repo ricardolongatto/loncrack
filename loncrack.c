26c8acda9dfb6e77dc6c97c857fb1554def5b1e2
pedroferreira <115036346+pedroferreiraa@users.noreply.github.com>
2023-01-11 16:01:17 -0300

    A senha passou a ter um tamanho variável, não é mais preciso inserir o salt, e algumas outras mudanças menores.

--- loncrack.c
+++ loncrack.c
@@ -1,47 +1,72 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#define BUFF 25
+#define SALT_BUFF 32 // Máximo valor do salt ~16
+#define HASH_BUFF 128 // Máximo valor do hash ~105
 
 int main(int argc, char *argv[])
 {
-	FILE *arq;
-	arq = fopen(argv[1],"r");
-	
-	// declarando as variáveis - vetores
-	char senha[BUFF + 50];
-	char salt[BUFF];
-	char comp[BUFF + 75];
-	char *result;
-	
-	// o hash vai ser armazenado em comp
-	printf("Digite o Hash completo\n");
-	scanf("%s", comp);
+	if (argc != 2) {
+		fprintf(stderr, "Modo de Uso: %s wordlist\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
-	// o salt vai ser armazenado em salt
-	printf("Digite o Salt\n");
-	scanf("%s",salt);
-	
-	int f = 0;
+	FILE *arq = fopen(argv[1],"r");
 
-	while(fscanf(arq, "%s", &senha) != EOF) {
+	// verificando se o fopen funcionou corretamente
+	if (!arq) {
+		fprintf(stderr, "O arquivo não existe.\n");
+		return EXIT_FAILURE;
+	}
+
+	// declarando as variáveis - vetoresa
+	int i = 0, j = 0, flag = 0;
+	char *senha = NULL;
+	char salt[SALT_BUFF] = { 0 };
+	char hash[HASH_BUFF] = { 0 };
+	char *result = NULL;
+	size_t n = 0;
+
+	// o hash vai ser armazenado na variável hash
+	printf("Digite o Hash completo:\n");
+	scanf("%s", hash);
+
+	// armazenando o salt
+	while (j < 3) {
+		salt[i] = hash[i];
+
+		if (salt[i] == '$') { ++j; }
+
+		++i;
+
+		// Evitar segmentation fault caso não encontre 3 '$'
+		if (i == HASH_BUFF) {
+			fprintf(stderr, "Formato do hash incorreto.\n");
+			return EXIT_FAILURE;
+		}
+	}
+
+	printf("Testando..\n");
+
+	while (getline(&senha, &n, arq) > 0) {
+		senha[strlen(senha) - 1] = 0; // eliminar o \n
 		result = (char *) crypt(senha, salt);
-			if (strcmp(comp,result)==0)
-			{
-				printf("Senha encontrada: %s \n", senha);
-				int f = 1;
-				return(0);
-			}
-			else
-			{
-				printf("Testando.. %s \n", senha);
-			}
+		if (strcmp(hash, result) == 0) {
+			flag = 1;
+			break;
+		}
 	}
-	
-	if(f==0)
-	{
+
+	if (flag == 0) {
 		printf("Senha não encontrada..\n");
+	} else {
+		printf("Senha encontrada: %s \n", senha);
 	}
+
+	free(senha);
+	fclose(arq);
+
+	return 0;
 }
